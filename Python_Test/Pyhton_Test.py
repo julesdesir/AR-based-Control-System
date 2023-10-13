b180f1a39f7dcafe2163b3df3e132125b5a93b8f
Romain Gras <romain.gras@etu.minesparis.psl.eu>
2023-10-13 11:14:50 +0200

    second commit

--- Python_Test/Pyhton_Test.py
+++ Python_Test/Pyhton_Test.py
@@ -1,27 +1,28 @@
 import socket
 
 host, port = "127.0.0.1", 25001
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((host, port))
 
 stay = True
 
-while (stay):
-    a = int(input())
-    b = int(input())
-    c = a+b
-    data = str(a)+","+str(b)+","+str(c)
-    print(data)
 
-    # Connect to the server and send the data
-    sock.sendall(data.encode("utf-8"))
-    response = sock.recv(1024).decode("utf-8")
-    print(response)
+try:
+    sock.connect((host, port))
+    while (stay):
+        a = int(input())
+        b = int(input())
+        c = a+b
+        data = str(a)+","+str(b)+","+str(c)
+        print(data)
 
-    exit = input("exit ? y/n ")
-    if exit == "y":
-        stay = False
+        # Connect to the server and send the data
+        sock.sendall(data.encode("utf-8"))
+        response = sock.recv(1024).decode("utf-8")
+        print(response)
 
+        exit = input("exit ? y/n ")
+        if exit == "y":
+            stay = False
 
-sock.close()
+finally:
+    sock.close()
